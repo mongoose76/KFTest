473dc69b612068d4d8e033a8d357bda71d58bee2
Doru Paraschiv <doru@snowglobegames.com>
2017-06-28 12:52:11 +0300

    Removed unused import

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,4 +1,4 @@
-﻿import { Component, ViewChild } from '@angular/core';
+﻿import { Component } from '@angular/core';
 import { Platform } from 'ionic-angular';
 import { NavController, AlertController } from 'ionic-angular';
 import { NewsPage } from '../news/news';
