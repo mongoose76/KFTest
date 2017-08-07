314b6ce60b89c975f09af732a9e62dab62280405
Doru Paraschiv <doru@snowglobegames.com>
2017-08-07 12:03:03 +0300

    First working company info widget

--- src/directives/company-info-widget/company-info-widget.ts
+++ src/directives/company-info-widget/company-info-widget.ts
@@ -1,5 +1,6 @@
-﻿import { Directive, Output, ElementRef, Input, EventEmitter } from '@angular/core';
+﻿import { Directive, ElementRef, Input } from '@angular/core';
 import { WidgetService } from '../../providers/widget-service';
+import { CompanyJSON } from '../../providers/companies-service';
 
 /**
  * Generated class for the CompanyInfoWidgetDirective directive.
@@ -12,19 +13,38 @@ import { WidgetService } from '../../providers/widget-service';
 })
 export class CompanyInfoWidgetDirective {
 
+    @Input() protected company_data: CompanyJSON;
+
     private tpl: string;
 
-    constructor(el: ElementRef, public widgets: WidgetService) {
-        console.log('Hello CompanyInfoWidgetDirective Directive');
+    constructor(public el: ElementRef, public widgets: WidgetService) {
+    }
+
+    /**
+     * Runs after the directives receives the input params
+     */
+    ngOnInit() {
 
         this.widgets.getCompanyInfo().then(tpl => {
-            //console.log(tpl);
-            //this.addDynamicElement(data);
-            this.tpl = <string>tpl;
 
-            el.nativeElement.insertAdjacentHTML('beforeend', this.tpl);
-            el.nativeElement.style.color = 'yellow';
+            this.tpl = <string>tpl;
+            this.el.nativeElement.insertAdjacentHTML('beforeend', this.applyData(this.tpl, this.company_data));
+            //el.nativeElement.style.color = 'yellow';
         });
     }
 
+    /**
+     * Replace template placeholders with actual company data
+     * @param tpl
+     * @param company
+     */
+    private applyData(tpl: string, company: CompanyJSON) {
+
+        for (var fld in company) {
+            console.log(company[fld]);
+            tpl = tpl.replace('{{company.' + fld + '}}', company[fld]);
+        }
+
+        return tpl;
+    }
 }
